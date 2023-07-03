5812e66f87a26448fb401c39a0750c11e4a3481a
huynamboz <teahuynam2003@gmail.com>
2023-07-03 07:33:22 +0700

    chore: change link ggform

--- components/layout/mainHome/FinalSection.vue
+++ components/layout/mainHome/FinalSection.vue
@@ -13,7 +13,7 @@
 					<p>ngay thôi nào !!</p>
 					
 				<a class="bg-[#bc52da] mt-10 px-5 py-3 rounded-3xl gap-2 flex items-center"
-				href="https://docs.google.com/forms/d/e/1FAIpQLSfKCsIDu2l1tvlmWSLE9owZ0LCSHbV_hBsqccrMSOwGy-y-Xg/viewform" 
+				href="https://docs.google.com/forms/d/e/1FAIpQLSdeI2PJRfIlYtbz19TJPwGMglGtwKlYW9wIdMYKfsFu0y-7bA/viewform" 
 				>Đăng ký ngay <i class="fi h-[18px] fi-rr-arrow-small-right"></i></a>
 				<a class=" "
 				href="https://discord.gg/Q6uQfHN9xa"
