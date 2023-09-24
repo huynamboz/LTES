464f0f4b5fb27ba81bcd93038122ebecf1d6d799
huynamboz <teahuynam2003@gmail.com>
2023-09-24 12:16:37 +0700

    chore: update custom path websocket

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client'
 const socket = io.connect(process.env.SOCKETIO_URL, {secure: true,forceNew: true,
+	path: "/ws/",
 	transports: ["websocket", "polling", "flashsocket"]})
 export default socket
