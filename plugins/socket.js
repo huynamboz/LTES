6bfee5aa0f0f20466113d45f550ad08dd3aa7d91
huynamboz <teahuynam2003@gmail.com>
2023-09-24 11:52:25 +0700

    bigfix: fix connection websocket

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
 const socket = io.connect(process.env.SOCKETIO_URL, {secure: true,forceNew: true,
-	transports: ["polling"]})
+	transports: ["websocket", "polling", "flashsocket"]})
 export default socket
