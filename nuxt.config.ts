f9099d35b8c5a7ed25bef85df6e5a99b39e40f16
huynamboz <teahuynam2003@gmail.com>
2024-07-09 18:04:03 +0700

    feat: optimize

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    buildAssetsDir: 'assets',
+    buildAssetsDir: '_assets',
     head: {
       link: [{ rel: 'icon', type: 'image/png', href: '/logo.jpg' }],
     },
